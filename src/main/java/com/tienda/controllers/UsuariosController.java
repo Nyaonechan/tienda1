99be8895e891190d627a33c418a2f64f01af3e2d
Nyaonechan <anixx89@gmail.com>
2022-10-21 13:47:49 +0200

    a

--- src/main/java/com/tienda/controllers/UsuariosController.java
+++ src/main/java/com/tienda/controllers/UsuariosController.java
@@ -71,6 +71,9 @@ public class UsuariosController {
 =======
 		System.out.println("llamando a controlador formularioRegistro");
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> f98ee11d036b823647517b441d4ebae59ff6e97d
+=======
 >>>>>>> f98ee11d036b823647517b441d4ebae59ff6e97d
 =======
 >>>>>>> f98ee11d036b823647517b441d4ebae59ff6e97d
